91af629e6df5ea325760108a5fff704593e7ed52
jimboweb <jimboweb@users.noreply.github.com>
2020-01-09 10:42:33 -0500

    okay did it again

---
+++ src/org/thelangschool/LinkedList/Main.java
@@ -0,0 +1,14 @@
+package org.thelangschool.LinkedList;
+
+class Main {
+
+    public Main(){
+        ListNode start = new ListNode("https://www.aol.com/");
+        ListNode second = new ListNode("https://www.aol.com/finance/?icid=aol.com-nav");
+        start.setNext(second);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
